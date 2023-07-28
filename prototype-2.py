2b9d1f0a6ae795e49118e3080c7d5b3eeb0dbab3
anonymous-specimen <62841119+anonymous-specimen@users.noreply.github.com>
2023-07-29 00:24:31 +0400

    Update prototype-2.py

--- prototype-2.py
+++ prototype-2.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 summarizer_object = pipeline("summarization", model = "facebook/bart-large-cnn")
  
 #Here is a list of all the articles text strings
-article_texts = ['hey jude is a person with big teeth', 'Jude doesnt know his teethe are biggie majiggy']
+article_texts = []
 
 text = " ".join(article_texts)
 
