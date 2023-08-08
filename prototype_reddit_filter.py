6641f93123d28c681683648e30a2b89f847f145e
anonymous-specimen <62841119+anonymous-specimen@users.noreply.github.com>
2023-08-09 02:27:40 +0400

    Create prototype_reddit_filter.py

---
+++ prototype_reddit_filter.py
@@ -0,0 +1,6 @@
+#subreddit_names = []
+#for index,row in itemsAsDataFrame.iterrows():
+ # if row['source_type'] == "Reddit":
+  # json_object = row.source
+  # equiv_dict = json.loads(json_object) #turn the retrieved json object into a python dictionary for easier use
+  #  subreddit_names.append(equiv_dict["subreddit_display"])
